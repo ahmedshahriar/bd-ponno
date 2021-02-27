d0ea09ea7f09431d9e7d39f4a29c820e9720bbec
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-02-27 09:11:51 +0600

    django core urls updated

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth', obtain_auth_token, name='get_token'),
+    path('api/', include('api.urls', namespace='api')),
 ]
