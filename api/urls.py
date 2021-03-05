3148098c07a7a37dc382515d902f726179ec0214
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-06 02:09:18 +0600

    django:search updated

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product_list'),
+    path('search/<str:name>/', views.ProductListView.as_view(), name='product_list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.pagination import PageNumberPagination
 
 from api.serializers import ProductSerializer
@@ -8,6 +8,20 @@ from products.models import Product
 # Create your views here.
 class ProductListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated, ]
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    queryset = Product.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+    pagination_class = PageNumberPagination
+
+
+class ProductSearchView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProductSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
     pagination_class = PageNumberPagination
