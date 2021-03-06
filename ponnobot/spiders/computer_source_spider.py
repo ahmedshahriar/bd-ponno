ca0f6daa0ea7644bc963f894d2032670d51f8a01
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-07 00:47:15 +0600

    scrapy:computer source image, title parsed

--- ponnobot/spiders/computer_source_spider.py
+++ ponnobot/spiders/computer_source_spider.py
@@ -2,6 +2,8 @@ import re
 
 import scrapy
 
+from ponnobot.items import ProductItem
+
 
 class ComputerSourceSpider(scrapy.Spider):
     name = "source"
@@ -14,4 +16,23 @@ class ComputerSourceSpider(scrapy.Spider):
 
     def begin_parse(self, response):
         urls = response.css('ul.categories_mega_menu > li > a ::attr("href")').getall()
-        print(urls, len(urls))
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        """
+        :param response:
+        :return: products and pagination callback
+        """
+
+        """ parse products """
+        product_page_links = response.css('div.single_product div.product_thumb a')
+        yield from response.follow_all(product_page_links, self.parse_product)
+
+    def parse_product(self, response):
+        item = ProductItem()
+        item['vendor'] = self.name
+        item['name'] = response.css('div.product_d_right h1 ::text').get()
+        item['product_url'] = response.url
+        item['image_url'] = response.css('meta[property="og:image"] ::attr("content")').get()
+        yield item
