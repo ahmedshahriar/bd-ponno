c9630bdff0e563e7a7817af49f6efa6a070bf013
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-09 03:40:21 +0600

    mke name,image,price,stock parsed

--- ponnobot/spiders/mke_spider.py
+++ ponnobot/spiders/mke_spider.py
@@ -36,7 +36,7 @@ class MKElectronicsSpider(scrapy.Spider):
         # """ parse test for a single product """
         single_product_url = 'https://www.mke.com.bd/samsung-ua78ks9000k-4k-curved-smart-tv'
 
-        yield response.follow(single_product_url)
+        yield response.follow(single_product_url, callback=self.parse_product)
 
         """ pagination """
         try:
@@ -51,4 +51,13 @@ class MKElectronicsSpider(scrapy.Spider):
         except ValueError as ve:
             print(ve)
 
-
+    def parse_product(self, response):
+        item = ProductItem()
+        item['vendor'] = self.name
+        item['product_url'] = response.url
+        item['name'] = response.css('h1.page-title span ::text').get()
+        item['brand'] = response.css('meta[property="product:brand"] ::attr("content") ').get()
+        item['image_url'] = response.css('meta[property="og:image"] ::attr("content") ').get()
+        item['price'] = response.css('span#product-price-8 span.price ::text').get()
+        item['in_stock'] = response.css('meta[property="product:availability"] ::attr("content") ').get()
+        yield item
