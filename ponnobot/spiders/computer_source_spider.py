89401238414869330991a54f5647e6f3cf26a0a4
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-11 06:41:11 +0600

    computer source: stock exception checked

--- ponnobot/spiders/computer_source_spider.py
+++ ponnobot/spiders/computer_source_spider.py
@@ -1,5 +1,4 @@
 import re
-from pprint import pprint
 
 import scrapy
 
@@ -10,17 +9,19 @@ class ComputerSourceSpider(scrapy.Spider):
     name = "source"
     allowed_domains = ['computersourcebd.com']
 
-    # start_urls = ['https://www.computersourcebd.com/']
+    # start_urls = ['https://www.computersourcebd.com']
     def start_requests(self):
         url = 'https://www.computersourcebd.com/'
         yield scrapy.Request(url=url, callback=self.begin_parse)
 
     def begin_parse(self, response):
         urls = response.css('ul.categories_mega_menu > li > a ::attr("href")').getall()
-        for url in urls:
+        # print(urls, len(urls))
+        for url in urls[:1]:
+            print(url)
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         """
         :param response:
         :return: products and pagination callback
@@ -28,6 +29,12 @@ class ComputerSourceSpider(scrapy.Spider):
 
         """ parse products """
         product_page_links = response.css('div.single_product div.product_thumb a')
+        # product_page_links = [
+        # 'https://computersourcebd.com/product/206/apacer-ah25b-16gb-usb-31-pen-drive-',
+        # 'https://computersourcebd.com/product/221/apacer-sdhc-uhs-1-16gb-class10-micro-sd-card',
+        # 'https://computersourcebd.com/product/234/epson-l130-ink-tank-printer'
+        # 'https://computersourcebd.com/product/536/apple-airpods'  # stock issue
+        # ]
         yield from response.follow_all(product_page_links, self.parse_product)
 
     def parse_product(self, response):
@@ -37,12 +44,22 @@ class ComputerSourceSpider(scrapy.Spider):
         item['product_url'] = response.url
         item['image_url'] = response.css('meta[property="og:image"] ::attr("content")').get()
         try:
-            _, price = (
-                re.findall(r'-?\d+\.?\d*', p.strip().replace(',', ''))[0] for p in
-                response.css('span.new_price ::text').getall())
+            price = [re.findall(r'-?\d+\.?\d*', p.strip().replace(',', ''))[0] for p in
+                     response.css('span.new_price ::text').getall()][-1]
             item['price'] = int(float(price))
         except ValueError as ve:
-            print(ve,'##############################################################################', response.url)
-        item['in_stock'] = False if response.css('p.stock.out-of-stock ::text').get() else True
-        # item.save()
-        yield item
+            print(ve, response.url)
+        except IndexError as ie:
+            print(ie, response.url)
+        try:
+            stock_status = response.css('div.product_d_right > p:last-of-type ::text').get()
+
+            if stock_status is None:
+                item['in_stock'] = True
+            else:
+                if 'Out' in stock_status:
+                    item['in_stock'] = False
+        except ValueError as ve:
+            print(ve, response.url)
+        if item['name'] is not None:
+            item.save()
