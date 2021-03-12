edeeec74a3a587fbd9570a6e154d651405cd2256
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-13 00:42:38 +0600

    othoba: pagination added

--- ponnobot/spiders/othoba_spider.py
+++ ponnobot/spiders/othoba_spider.py
@@ -14,4 +14,29 @@ class OthobaSpider(scrapy.Spider):
 
     def begin_parse(self, response):
         urls = response.css('ul li.lnkHeading a ::attr("href")').getall()
-        print(len(urls), urls)
+        # print(len(urls),urls)
+        for url in urls:
+            url = 'https://www.othoba.com' + str(url)
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response, **kwargs):
+        """
+        :param response:
+        :return: products and pagination callback
+        """
+        """ parse products """
+        # product_page_links = response.css('div.product-item div  a ')
+        # yield from response.follow_all(product_page_links, self.parse_product)
+
+        """ pagination """
+        try:
+            pagination_links = response.css('div.pager ul li.next-page a ::attr("href")').get()
+            yield response.follow(pagination_links, self.parse)
+        except IndexError as ie:
+            # logging.info(ie, logging.WARN)
+            print(ie)
+        except TypeError as te:
+            # logging.info(te, logging.WARN)
+            print(te)
+        except ValueError as ve:
+            print(ve)
