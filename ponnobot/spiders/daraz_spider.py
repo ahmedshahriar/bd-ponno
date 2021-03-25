b4bea8f125c99c335c43e844b2a563698d72b533
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-03-25 19:52:09 +0600

    daraz product json parsed

--- ponnobot/spiders/daraz_spider.py
+++ ponnobot/spiders/daraz_spider.py
@@ -15,7 +15,30 @@ class DarazSpider(scrapy.Spider):
 
     def begin_parse(self, response):
         urls = response.css('ul.lzd-site-menu-sub li.lzd-site-menu-sub-item > a::attr("href")').getall()
-        print(len(urls))
         for url in urls:
             url = "https:" + str(url)
-            print(url)
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response, **kwargs):
+        """
+        :param response:
+        :return: products and pagination callback
+        """
+        """ parse products """
+        raw_product_list = re.compile(r'window.pageData=(.*)</script>').search(response.text)
+        product_list = json.loads(raw_product_list.group(1).strip())['mods']['listItems']
+        for product in product_list:
+            yield product
+
+        """ pagination """
+        try:
+            pagination_links = response.css('link[rel="next"] ::attr("href")').get()
+            yield response.follow(pagination_links, self.parse)
+        except IndexError as ie:
+            # logging.info(ie, logging.WARN)
+            print(ie)
+        except TypeError as te:
+            # logging.info(te, logging.WARN)
+            print(te)
+        except ValueError as ve:
+            print(ve)
