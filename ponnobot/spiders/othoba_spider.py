7bc3c6b671ea5e00b2b573136b249aa26152bfd4
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-04-28 19:52:48 +0600

    othoba xrawler updated

--- ponnobot/spiders/othoba_spider.py
+++ ponnobot/spiders/othoba_spider.py
@@ -11,20 +11,20 @@ class OthobaSpider(scrapy.Spider):
     name = "othoba"
     allowed_domains = ['othoba.com']
 
-    start_urls = ['https://www.othoba.com/electronics']
+    # start_urls = ['https://www.othoba.com/electronics']
 
-    # def start_requests(self):
-    #     url = 'https://www.othoba.com/'
-    #
-    #     yield scrapy.Request(url=url, callback=self.begin_parse)
-    #
-    # def begin_parse(self, response):
-    #     urls = response.css('ul li.lnkHeading a ::attr("href")').getall()
-    #     # print(len(urls), urls)
-    #     for url in urls[:1]:
-    #         url = 'https://www.othoba.com' + str(url)
-    #         # print(url, sep='\n')
-    #         yield scrapy.Request(url=url, callback=self.parse)
+    def start_requests(self):
+        url = 'https://www.othoba.com/'
+
+        yield scrapy.Request(url=url, callback=self.begin_parse)
+
+    def begin_parse(self, response):
+        urls = response.css('ul li.lnkHeading a ::attr("href")').getall()
+        # print(len(urls), urls)
+        for url in urls[:1]:
+            url = 'https://www.othoba.com' + str(url)
+            # print(url, sep='\n')
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
         """
@@ -34,25 +34,25 @@ class OthobaSpider(scrapy.Spider):
 
         # todo sold out products from thumbnail
         """ parse products """
-        # product_page_links = response.css('div.product-item > div.picture  a ')
-        # yield from response.follow_all(product_page_links, self.parse_product)
+        product_page_links = response.css('div.product-item > div.picture  a ')
+        yield from response.follow_all(product_page_links, self.parse_product)
 
         """ parse test for a single product """
-        single_product_url = 'https://www.othoba.com/moveable-luxurious-garden-umbrella-60-aku00055' #stock issue
-        yield response.follow(single_product_url, callback=self.parse_product)
+        # single_product_url = 'https://www.othoba.com/moveable-luxurious-garden-umbrella-60-aku00055' #stock issue
+        # yield response.follow(single_product_url, callback=self.parse_product)
 
         """ pagination """
-        # try:
-        #     pagination_links = response.css('div.pager ul li.next-page a ::attr("href")').get()
-        #     yield response.follow(pagination_links, self.parse)
-        # except IndexError as ie:
-        #     # logging.info(ie, logging.WARN)
-        #     print(ie)
-        # except TypeError as te:
-        #     # logging.info(te, logging.WARN)
-        #     print(te)
-        # except ValueError as ve:
-        #     print(ve)
+        try:
+            pagination_links = response.css('div.pager ul li.next-page a ::attr("href")').get()
+            yield response.follow(pagination_links, self.parse)
+        except IndexError as ie:
+            # logging.info(ie, logging.WARN)
+            print(ie)
+        except TypeError as te:
+            # logging.info(te, logging.WARN)
+            print(te)
+        except ValueError as ve:
+            print(ve)
 
     def parse_product(self, response):
         """
@@ -61,26 +61,31 @@ class OthobaSpider(scrapy.Spider):
         """
         tag_list = []
         item = ProductItem()
+
         try:
             item['vendor'] = self.name
             item['product_url'] = response.url
             item['name'] = response.css('h1[itemprop="name"] ::text').get().strip()
             item['image_url'] = response.css('meta[property="og:image"] ::attr("content") ').get()
             item['price'] = int(float(response.css('div.product-price span ::attr("content")').get()))
-            item['in_stock'] = False if response.css('span.sold-out').get() else True
+            item['in_stock'] = 0 if response.css('span.sold-out').get() else 1
+            product_vendor = response.css('div.product-vendor a ::text').get()
+            if product_vendor:
+                tag_list.append(slugify(product_vendor, allow_unicode=True) if product_vendor else "")
             categories = response.css('div.breadcrumb a span ::text').getall()[1:]
-            if len(categories) > 3:
-                tag_list.extend([slugify(category, allow_unicode=True) for category in categories[:]])
-            category_obj = None
-            # try:
-            #     category_obj = Category.objects.get(slug=slugify(categories[2], allow_unicode=True))
-            #     logging.info("category already exists")
-            # except Category.DoesNotExist:
-            #     category_obj = Category(name=categories[1], slug=slugify(categories[2], allow_unicode=True))
-            #     category_obj.save()
+
+            try:
+                Category.objects.get(slug=slugify(categories[1], allow_unicode=True))
+                logging.info("category already exists")
+            except Category.DoesNotExist:
+                category_obj = Category(name=categories[1], slug=slugify(categories[1], allow_unicode=True))
+                category_obj.save()
+            categories.remove(categories[1])
+            if len(categories) > 1:
+                tag_list.extend([slugify(category, allow_unicode=True) for category in categories])
             item['tags'] = [{"name": value} for value in tag_list]
         except Exception as e:
             print(e, response.url)
         if item['name'] is not None:
-            # item.save()
-            print(item, categories)
+            item.save()
+            # print(item, '###\n', category_obj)
