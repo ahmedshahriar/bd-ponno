066304360dad12dd41f99fa69c0e3214432c26dc
Ahmed Shahriar Sakib <ahmed.shahriar.sakib@gmail.com>
2021-04-30 23:57:04 +0600

    rokomari author , tag , category added

--- ponnobot/spiders/rokomari_spider.py
+++ ponnobot/spiders/rokomari_spider.py
@@ -6,17 +6,18 @@ from ponnobot.items import ProductItem
 class RokomariBookSpider(scrapy.Spider):
     name = "rokomari"
     allowed_domains = ['rokomari.com']
+    start_urls = ['https://www.rokomari.com']
 
-    def start_requests(self):
-        url = 'https://www.rokomari.com/book/categories'
-        yield scrapy.Request(url=url, callback=self.begin_parse)
-
-    def begin_parse(self, response):
-        urls = response.css('div.pFIrstCatCaroItem a ::attr("href")').getall()
-        print(len(urls), urls)
-        for url in urls[1:]:
-            url = 'https://www.rokomari.com' + str(url)
-            yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    #     url = 'https://www.rokomari.com/book/categories'
+    #     yield scrapy.Request(url=url, callback=self.begin_parse)
+    #
+    # def begin_parse(self, response):
+    #     urls = response.css('div.pFIrstCatCaroItem a ::attr("href")').getall()
+    #     print(len(urls), urls)
+    #     for url in urls[1:]:
+    #         url = 'https://www.rokomari.com' + str(url)
+    #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
         """
@@ -25,13 +26,13 @@ class RokomariBookSpider(scrapy.Spider):
         """
         """ parse products """
 
-        product_page_links = response.css('div.book-list-wrapper div.home-details-btn-wrapper  a ')
-        yield from response.follow_all(product_page_links, self.parse_product)
+        # product_page_links = response.css('div.book-list-wrapper div.home-details-btn-wrapper  a ')
+        # yield from response.follow_all(product_page_links, self.parse_product)
 
         """ parse test for a single product """
-        # single_product_url = 'https://www.rokomari.com/book/132098/a-mind-for-numbers'
-        # yield response.follow(single_product_url,
-        #                       callback=self.parse_product)
+        single_product_url = 'https://www.rokomari.com/book/132098/a-mind-for-numbers'
+        yield response.follow(single_product_url,
+                              callback=self.parse_product)
 
         """ pagination """
         try:
@@ -52,12 +53,24 @@ class RokomariBookSpider(scrapy.Spider):
         :return: product details dictionary
         """
         item = ProductItem()
+        tag_list = []
+        category_obj = None
+
         item['vendor'] = self.name
         item['product_url'] = response.url
         item['name'] = response.css('div.details-book-main-info__header h1 ::text').get().strip()
         item['price'] = int(
             float(response.css('meta[property="product:price:amount"] ::attr("content")').get().strip()))
-        item['in_stock'] = False if 'in' in response.css(
-            'meta[property="product:availability"] ::attr("content")').get().strip().lower() else True
+        item['in_stock'] = 0 if 'in' in response.css(
+            'meta[property="product:availability"] ::attr("content")').get().strip().lower() else 1
         item['image_url'] = response.css('meta[property="og:image"] ::attr("content")').get().strip()
-        yield item
+        publisher = response.css('td.publisher-link a ::text').get().strip()
+        # todo check for brand name in bangla
+        # response.css('meta[property="product:brand"] ::attr("content")').get().strip()
+
+        tag_list.append(publisher)
+        category = response.css('div.details-book-info__content-category a.ml-2 ::text').get().strip()
+        author_name = response.css('p.details-book-info__content-author a ::text').get().strip()
+        tag_list.append(author_name)
+        print(category, item, tag_list)
+
